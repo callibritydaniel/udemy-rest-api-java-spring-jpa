78fab853b452775392ef30326e47890d8ee05f24
Daniel Silva <daniel.silva@callibrity.com>
2023-01-13 12:48:05 -0500

    Improved it with EJ's help.

--- restapi/src/test/java/com/example/callibritydaniel/restapi/controller/StudentControllerTests.java
+++ restapi/src/test/java/com/example/callibritydaniel/restapi/controller/StudentControllerTests.java
@@ -1,13 +1,14 @@
 package com.example.callibritydaniel.restapi.controller;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.verify;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
@@ -18,13 +19,23 @@ public class StudentControllerTests {
 
     @Autowired
     private TestRestTemplate restTemplate;
+    @MockBean
+    private StudentController mockController;
 
-	@Test
-	public void testGetStudent() {
-		StudentController c = new StudentController();
+    @Test
+    public void testGetStudent() {
+        StudentController c = new StudentController();
         assertEquals("Hello Student", c.getStudent());
-        
-        assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/api/student/get", String.class)).contains("Hello Student");
-	}
+    }
+
+    @Test
+    public void testGetStudentAPIMapping() {
+        verifyMapping("/api/student/get");
+    }
+
+    private void verifyMapping(String mapping) {
+        restTemplate.getForObject("http://localhost:"+port+mapping, String.class);
+        verify(mockController).getStudent();
+    }
     
 }
